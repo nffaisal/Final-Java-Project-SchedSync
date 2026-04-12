93356735e6cfde417d128c6eddc1a110a053c0af
Noor <noorffatima0045@gmail.com>
2026-04-12 15:54:57 +0500

    first commit

---
+++ src/main/java/com/timetable/Main.java
@@ -0,0 +1,24 @@
+package com.timetable;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("SchedSync is working!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("SchedSync");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
